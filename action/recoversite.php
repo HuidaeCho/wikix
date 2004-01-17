92b5b52c68be52dad5d833780d0e10f275f8af6f
Huidae Cho <grass4u@gmail.com>
2004-01-17 10:22:42 +0000

    fixed

--- action/recoversite.php
+++ action/recoversite.php
@@ -10,7 +10,7 @@ if(!is_site_locked()){
 	return;
 }
 
-$query = "select id, version, tversion, tname from ${db_}page
+$query = "select id, name, version, tversion, tname from ${db_}page
 				where tversion>0 order by id";
 $result = pm_query($db, $query);
 $n = pm_num_rows($result);
@@ -18,9 +18,22 @@ $n = pm_num_rows($result);
 for($i=0; $i<$n; $i++){
 	$data = pm_fetch_array($result, $i);
 	$id = $data['id'];
+	$name = addslashes($data['name']);
 	$version = $data['version'];
 	$tversion = $data['tversion'];
 	$tname = addslashes($data['tname']);
+	if($name != $tname){
+		if(pageid($tname))
+			warn("$tname: Page already exists.");
+		else
+		if(pageid0($tname))
+			warn("$tname: Deleted page exists.");
+		else{
+			$query = "update ${db_}page set name='$tname'
+						where id=$id";
+			$result0 = pm_query($db, $query);
+		}
+	}
 	if($tversion == $version)
 		continue;
 	$content = page_content($id, $tversion);
@@ -28,8 +41,7 @@ for($i=0; $i<$n; $i++){
 	$query = "update ${db_}data set content='$content'
 				where id=$id and version=$tversion";
 	$result0 = pm_query($db, $query);
-	$query = "update ${db_}page set version=$tversion, name='$tname'
-				where id=$id";
+	$query = "update ${db_}page set version=$tversion where id=$id";
 	$result0 = pm_query($db, $query);
 	$query = "delete from ${db_}data where id=$id and version>$tversion";
 	$result0 = pm_query($db, $query);
